8efb391201eeeefdd0a036f8f4b3ddd86995c878
xiu <gaoxiu333@gmail.com>
2024-05-05 19:19:41 +0800

    about

--- packages/template-vue-antd/src/views/about/index.vue
+++ packages/template-vue-antd/src/views/about/index.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import pkg from '../../../package.json'
+const { version, dependencies, devDependencies } = pkg
+</script>
 <template>
-    关于
+    <a-row :gutter="[16, 16]">
+        <a-col :span="12">
+            <a-descriptions title="生产依赖" bordered :column="1">
+                <a-descriptions-item v-for="(value, key) in dependencies" :key="value" :label="key">
+                    {{ value }}
+                </a-descriptions-item>
+            </a-descriptions>
+        </a-col>
+        <a-col :span="12">
+            <a-descriptions title="开发环境依赖" bordered :column="1">
+                <a-descriptions-item v-for="(value, key) in devDependencies" :key="value" :label="key">
+                    {{ value }}
+                </a-descriptions-item>
+            </a-descriptions>
+        </a-col>
+    </a-row>
+
 </template>
