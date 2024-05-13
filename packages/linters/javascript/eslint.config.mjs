ebb51bbec1ab99ea042d36d3085e62e886394778
xiu <gaoxiu333@gmail.com>
2024-05-13 21:35:07 +0800

    feat: 添加注释说明

--- packages/linters/javascript/eslint.config.mjs
+++ packages/linters/javascript/eslint.config.mjs
@@ -1,13 +1,19 @@
 import globals from "globals";
 import js from "@eslint/js";
 import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
-
+/**
+ * 配置包含
+ * 1. browser 全局变量
+ * 2. es2021 全局变量
+ * 3. node 全局变量
+ * 4. 支持 JSX
+ * 5. 仅支持 ESM
+ */
 /** @type {import('eslint').Linter.Config} */
 export default [
   {
     name: "JavaScript",
     languageOptions: {
-      ecmaVersion: 2022,
       globals: {
         ...globals.browser,
         ...globals.es2021,
@@ -20,16 +26,15 @@ export default [
         ecmaFeatures: {
           jsx: true,
         },
-        ecmaVersion: 2022,
-        sourceType: "module",
       },
+      ecmaVersion: 2022,
       sourceType: "module",
     },
     linterOptions: {
-      reportUnusedDisableDirectives: true,
+      reportUnusedDisableDirectives: true, // 指示是否以及如何跟踪和报告未使用的禁用和启用指令
     },
     plugins: {
-      unusedImports: eslintPluginUnusedImports,
+      unusedImports: eslintPluginUnusedImports, // 查找并删除未使用的 es6 模块导入
     },
     // 针对 unused-imports 的配置
     rules: {
